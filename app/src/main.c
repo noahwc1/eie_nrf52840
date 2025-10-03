e1c5f90775a88258a7b5e9a4651610407a2df5b6
Noah Weir Chaba <noahweirchaba@gmail.com>
2025-10-03 13:40:18 -0600

    circle lights

--- app/src/main.c
+++ app/src/main.c
@@ -54,11 +54,19 @@ int main(void) {
     }
 
     while (1){
+      
         gpio_pin_toggle_dt(&led0);
+        k_msleep(300);
+        gpio_pin_configure_dt(&led0, GPIO_OUTPUT_INACTIVE);
         gpio_pin_toggle_dt(&led1);
-        gpio_pin_toggle_dt(&led2);
+        k_msleep(300);
+        gpio_pin_configure_dt(&led1, GPIO_OUTPUT_INACTIVE);
         gpio_pin_toggle_dt(&led3);
-        k_msleep(1000);
+        k_msleep(300);
+        gpio_pin_configure_dt(&led3, GPIO_OUTPUT_INACTIVE);
+        gpio_pin_toggle_dt(&led2);
+        k_msleep(300);
+        gpio_pin_configure_dt(&led2, GPIO_OUTPUT_INACTIVE);
     }
     return 0;
 
