5cd01acddf786bddab4888dbbcf2880ae6a6e0f9
Noah Weir Chaba <noahweirchaba@gmail.com>
2025-10-16 20:31:11 -0600

    LED API

--- app/src/main.c
+++ app/src/main.c
@@ -8,39 +8,25 @@
 #include <zephyr/sys/printk.h>
 #include <inttypes.h>
 
-#define  SLEEP_TIME_MS   1000
+#include "LED.h"
+#include "BTN.h"
 
-#define SW0_NODE DT_ALIAS(sw0)
-static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(SW0_NODE, gpios);
+#define  SLEEP_TIME_MS   1
 
-static struct gpio_callback button_isr_data;
-
-void button_isr(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
+int main(void)
 {
-    printk("Pressed 0 pressed!\n");
-}
-
-int main(void) {
-  int ret;
-
-  if (!gpio_is_ready_dt(&button)) {
+    if (0 > LED_init()) {
+        return 0;
+    }
+    if (0 > BTN_init()) {
+        return 0;
+    }
+    while (1) {
+        if (BTN_check_clear_pressed(BTN0)){
+            LED_toggle(LED0);
+            printk("Button 0 pressed\n");
+        }
+        k_msleep(SLEEP_TIME_MS);
+    }
     return 0;
-  }
-
-  ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
-    if (0 > ret) {
-        return 0;
-    }
-  ret = gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_TO_ACTIVE);
-    if (0 > ret) {
-        return 0;
-    }
-
-    gpio_init_callback(&button_isr_data, button_isr, BIT(button.pin));
-    gpio_add_callback(button.port, &button_isr_data);
-
-  while(1) {
-    
-  }
-	return 0;
 }
