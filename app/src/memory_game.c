ba141b01e24307bfec582d948754078c48a74385
Noah Weir Chaba <noahweirchaba@gmail.com>
2026-02-12 20:32:30 -0700

    starting to add actions in states

--- app/src/memory_game.c
+++ app/src/memory_game.c
@@ -45,12 +45,13 @@ enum memory_game_states {
   WINNER_STATE,
 };
 
-
+// Creates structure type for variables tracked during states
 typedef struct {
     // context variable used by Zephyr to track state machine state must be first
     struct smf_ctx ctx;
     enum memory_game_states current_state;
     uint16_t count;
+    uint16_t round;
     
     int led3_on;
     int64_t last_toggle_ms;
@@ -59,6 +60,8 @@ typedef struct {
 
 } state_object_t;
 
+// Declaring scoped variable for code from struct type
+static state_object_t state_object;
 
 
 
@@ -91,11 +94,12 @@ static const struct smf_state game_states[] = {
 }
 
 
-static state_object_t  state_object;
+
 
 // Initialize memory game for start
 void memory_game_innit() {
-  state_object.count = 0;
+  state_object.round = 0;
+  state_object.current_state = STANDBY_STATE;
   smf_set_initial(SMF_CTX(&state_object), &states[STANDBY_STATE]);
 
 
@@ -107,3 +111,67 @@ int state_machine_run() {
 
 
 // Local Variables
+
+typedef struct {
+  int player_sequence = {-1,-1,-1,-1, -1,-1,-1,-1,-1,-1};
+  int *ptr_player_sequence = player_sequence;
+  int player_errorflag = 1;
+} player1;
+
+typedef struct {
+  int player_sequence = {-1,-1,-1,-1, -1,-1,-1,-1,-1,-1};
+  int *ptr_player_sequence = player_sequence;
+  int player_errorflag = 1;
+} player2;
+
+
+int sequence_length = 4;
+int sequence_index = 0;
+
+int enter_sequence(int)
+int enter_sequence(int which_button, struct player, int next_state){
+  if (player.player_sequence[sequence_index] == -1)
+        player.player_sequence[sequence_index] = which_button;
+  else {
+      if (player.player_sequence[sequence_index] != which_button){
+        player_errorflag = 0;
+        smf_state(SMF_CTX(&state_object), &states[next_state]);
+        return;
+      }
+    }    
+  }
+
+// Memory Game State functions, what happens in each state
+
+static void standby_state(void* 0){
+  state_object.round = 0;
+  state_object.current_state = STANDBY_STATE;
+}
+
+static enum smf_state_result standby_state_run(void* o){
+
+// If BTN 0 is pressed go into player 1 enter
+if (BTN_check_clear_pressed(BTN0)){
+  smf_state(SMF_CTX(&state_object), &states[PLAYER1_ENTER]);
+  return SMF_EVENT_HANDLED;
+}
+
+return SMF_EVENT_HANDLED;
+}
+
+static void player1_enter_state(void* o){
+  state_object.count = 0;
+  state_object.current_state = PLAYER1_ENTER;
+
+}
+
+static enum smf_state_result player1_enter_run(void* o){
+  
+  
+  if (BTN_check_clear_pressed(BTN0) ){
+        return SMF_RETURN
+        
+  }
+
+
+}
